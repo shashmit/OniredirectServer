ba0c7def1178036a22bee71792117b11fa9f5e1c
Shashmit <shaashmit@gmail.com>
2024-07-17 18:43:07 +0530

    notify

---
+++ handlers/careContextHandle/notifyUser.js
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
+import config from '../../config/temp.js';
+
+async function notifyUser(req, res) {
+  const { phoneNo } = req.body;
+
+  try {
+    const requestId = uuidv4();
+    const timestamp = new Date().toISOString();
+
+    const body = {
+      requestId: requestId,
+      timestamp: timestamp,
+      notification: {
+        phoneNo: phoneNo,
+        hip: {
+          name: "Onicares",
+          id: "IN3410000818"
+        }
+      }
+    };
+
+    const response = await axios.post(
+      'https://dev.abdm.gov.in/gateway/v0.5/patients/sms/notify2',
+      body,
+      config.gwApiConfig // Assuming this is defined in your config file
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+}
+
+export { notifyUser };
--- routes/careContext.js
+++ routes/careContext.js
@@ -1,9 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { addCareContext } from '../handlers/careContextHandle/addcareContext.js';
-
+import { notifyUser } from '../handlers/careContextHandle/notifyUser.js';
 
 router.post("/addCareContext", addCareContext);
+router.post("/notify", notifyUser);
 
 
 export default router;
