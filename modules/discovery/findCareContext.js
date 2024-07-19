2688ed31e732dc4a05dc261f44fd1a9ddabcadc7
Shashmit <shaashmit@gmail.com>
2024-07-19 14:29:06 +0530

    Update findCareContext.js

--- modules/discovery/findCareContext.js
+++ modules/discovery/findCareContext.js
@@ -2,19 +2,16 @@ import config from '../../config/temp.js';
 
 export default async function findCareContext(data) {
   const matchingPatient = config.OTPDATABASE.find(c => c.transactionId === data.transactionId);
-  console.log("matching patient", config.OTPDATABASE);
   if (!matchingPatient) {
     throw new Error("No Care Context Found");
   }
   
   const requestedReferenceNumbers = data.patient[0].careContexts.map(cc => cc.referenceNumber);
   let filteredCareContexts;
-  console.log("MAtching patient", matchingPatient);
   
   try{
-    filteredCareContexts = matchingPatient.patient.careContexts.filter(cc =>
-      requestedReferenceNumbers.includes(cc.referenceNumber)
-    );
+    filteredCareContexts = matchingPatient.careContexts;
+    console.log("findCareContext",JSON.stringify(filteredCareContexts));
   } catch (e) {
     throw new Error(e);
   }
