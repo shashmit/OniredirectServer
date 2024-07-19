e4619287eded78eeaf1c0ea13307df4c7dd6ea78
Shashmit <shaashmit@gmail.com>
2024-07-19 13:54:13 +0530

    Update findCareContext.js

--- modules/discovery/findCareContext.js
+++ modules/discovery/findCareContext.js
@@ -1,23 +1,25 @@
 import config from '../../config/temp.js';
 
-
 export default async function findCareContext(data) {
-  const matchingPatients = config.OTPDATABASE.find(c => c.transactionId === data.transactionId);
-  if(!careContext){
+  const matchingPatient = config.OTPDATABASE.find(c => c.transactionId === data.transactionId);
+  
+  if (!matchingPatient) {
     throw new Error("No Care Context Found");
   }
-  if(careContext){
-    const requestedReferenceNumbers = data.patient[0].careContexts.map(cc => cc.referenceNumber);
-    console.log(requestedReferenceNumbers);
-    const filteredCareContexts = matchingPatient.careContexts.filter(cc => 
-      requestedReferenceNumbers.includes(cc.referenceNumber)
-    );
-    if(filteredCareContexts.length === 0){
-      throw new Error("No Care Context Found");
-    }
-    return {
-      ...matchingPatients,
-      careContexts: filteredCareContexts
-    }
+  
+  const requestedReferenceNumbers = data.patient[0].careContexts.map(cc => cc.referenceNumber);
+  console.log(requestedReferenceNumbers);
+  
+  const filteredCareContexts = matchingPatient.careContexts.filter(cc =>
+    requestedReferenceNumbers.includes(cc.referenceNumber)
+  );
+  
+  if (filteredCareContexts.length === 0) {
+    throw new Error("No Care Context Found");
   }
+  
+  return {
+    ...matchingPatient,
+    careContexts: filteredCareContexts
+  };
 }
