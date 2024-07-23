3d5e2e7f6979f0396642ba790297b22b6e6cb9b7
Shashmit <shaashmit@gmail.com>
2024-07-24 01:21:21 +0530

    Update generateToken.js

--- handlers/careContextHandle/generateToken.js
+++ handlers/careContextHandle/generateToken.js
@@ -3,29 +3,43 @@ import axios from 'axios';
 import config from '../../config/temp.js';
 import refreshAccessToken from '../../utils/refreshToken.js';
 
+
 async function generateToken(req, res) {
-  const { abhaNumber, abhaAddress, name, gender, dob} = req.body
+  const { abhaNumber, abhaAddress, name, gender, dob } = req.body;
+  
   if (!config.accessToken) {
-    await refreshAccessToken();
+      try {
+          await refreshAccessToken();
+      } catch (error) {
+          console.error('Error refreshing token:', error.message);
+          return res.status(500).json({ status: "error", message: "Failed to refresh access token" });
+      }
   }
-  const body ={
-    abhaNumber: abhaNumber,
-    abhaAddress: abhaAddress,
-    name: name,
-    gender: gender,
-    dob: dob
-  }
-  try{
-    const response = await axios.post(
-      'https://dev.abdm.gov.in/hiecm/api/v3/token/generate-token',
-      body,
-      config.gwApiConfig
-    );
-    res.status(200).json(response.data);
-  }catch(err){
-    console.error('Error:', err.message);
-    res.status(500).json({ status: "error", message: "Internal server error" });
+  
+  const body = {
+      abhaNumber,
+      abhaAddress,
+      name,
+      gender,
+      dob
+  };
+  
+  try {
+      const response = await axios.post(
+          'https://dev.abdm.gov.in/hiecm/api/v3/token/generate-token',
+          body,
+          config.gwApiConfig
+      );
+      res.status(200).json(response.data);
+  } catch (err) {
+      console.error('Error:', err.message);
+      if (err.response) {
+          res.status(err.response.status).json({ status: "error", message: err.response.data.message || "Error from server" });
+      } else {
+          res.status(500).json({ status: "error", message: "Internal server error" });
+      }
   }
 }
+
 
 export default generateToken;
